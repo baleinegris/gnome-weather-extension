1e5a758e358970f0ec94f12bb26b82c091337144
Oscar Heath <oscar.w.heath@gmail.com>
2025-08-29 22:08:16 -0400

    Set up temperature fetching logic

--- extension.js
+++ extension.js
@@ -1,21 +1,36 @@
 import St from 'gi://St';
+import Clutter from 'gi://Clutter';
 
 import {Extension} from 'resource:///org/gnome/shell/extensions/extension.js';
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
+import { LibSoup } from './libsoup.js';
+
 
 
 export default class ExampleExtension extends Extension {
-    enable() {
-        // Create a panel button
+    constructor(metadata) {
+        super(metadata);
+        this._indicator = null;        
+    }
+
+    async enable() {
+        const params = {
+            "latitude": "43.9779",
+            "longitude": "-79.9572",
+            "hourly": "temperature_2m",
+        };
         this._indicator = new PanelMenu.Button(0.0, this.metadata.name, false);
 
-        // Add an icon
-        const icon = new St.Icon({
-            icon_name: 'face-laugh-symbolic',
-            style_class: 'system-status-icon',
-        });
-        this._indicator.add_child(icon);
+        let soup = new LibSoup();
+        const responses = await soup.fetchJson('https://api.open-meteo.com/v1/forecast', params);
+
+        console.log(responses);
+
+        this._indicator.add_child(new St.Label({ 
+            text: `Temperature: ${responses.body.hourly.temperature_2m[0]}°C`,
+            y_align: Clutter.ActorAlign.CENTER 
+        }));
 
         // Add the indicator to the panel
         Main.panel.addToStatusArea(this.uuid, this._indicator);
